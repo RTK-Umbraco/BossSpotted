ce2b314fdf4f9ac6e212ca23a27c5fb7a2b54184
RTK-Umbraco <87420491+RTK-Umbraco@users.noreply.github.com>
2023-01-16 10:28:38 +0100

    Add project files.

---
+++ BossSpotted/Controllers/BossSpottedController.cs
@@ -0,0 +1,21 @@
+﻿using BossSpotted.Hubs;
+using BossSpotted.Hubs.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BossSpotted.Controllers
+{
+    public class BossSpottedController : Controller
+    {
+        private readonly IBossSpottedHub _bossSpottedHub;
+        public BossSpottedController(IBossSpottedHub bossSpottedHub)
+        {
+            _bossSpottedHub = bossSpottedHub;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //this.HubContext.Clients.All.InvokeAsync("Completed");
+            await _bossSpottedHub.SendMessage("Daniel", "Benjamin");
+            return View();
+        }
+    }
+}
---
+++ BossSpotted/Hubs/Interface/IBossSpottedHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BossSpotted.Hubs.Interface
+{
+    public interface IBossSpottedHub
+    {
+        Task<IActionResult> Index();
+    }
+}
