84a4e76690aa6a62db0746efa363524e8771ead0
Daniel Nygaard Vuust28 <daniel.vuust@gmail.com>
2023-01-17 08:57:22 +0100

    Check if signalR is connected and display button color accordingly

--- BossSpotted/wwwroot/js/bossSpotted.js
+++ BossSpotted/wwwroot/js/bossSpotted.js
@@ -1,9 +1,13 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/bossSpottedHub").build();
-
+isSignalConnected(false);
 connection.on("BossHasBeenSpotted", function () {
     blinkScreen();
 });
 
-connection.start();
+connection.start().then(function () {
+    isSignalConnected(true);
+    }).catch(function (err) {
+        return console.error(err.toString());
+});
